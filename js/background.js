a4ff742cf9bd77af29f119c0e95edda22ce7dc89
Rosie-Brigham <rosiebrigham@gmail.comm>
2018-11-11 17:45:29 +0000

    removes solution

--- js/background.js
+++ js/background.js
@@ -12,7 +12,6 @@ $( document ).ready(function(){
   $('h1').on('click', function () {
 
     // 1. log to the console that you have clicked the button
-    console.log("the button has been clicked!")
 
     // this selects a randon background from above and stores it as the variable 'randomBackground'
     var randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
@@ -21,14 +20,12 @@ $( document ).ready(function(){
     
     
     // 2. using jquery, change the image url of the body background to the new one saved above
-    $('body').css('background-image', imageUrl)
 
     // EXTENSION TASK!
     // 3. Change the text of the 'span' to be that of the photographer
     //   (hint, you should re-use randomBackgroun)
     // Double points if you replace '-' between their names with a space
-    var artist = randomBackground.replace('-', ' ')
-    $('span').text(artist)
 
+    
   }) 
 }) // here endeth the function
